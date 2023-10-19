5a6394108577751b15addd6dc90099bbdbeb852c
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2023-10-18 20:10:28 -0400

    Added license to main file

--- Sources/RNGTool-Plus/RNGTool_Plus.swift
+++ Sources/RNGTool-Plus/RNGTool_Plus.swift
@@ -1,10 +1,30 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+/*  RNGTool Plus "RNGTool_Plus.swift"
+MIT License
+
+Copyright (c) 2023 NCX Programming
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE */
 
 import SwiftCrossUI
 import GtkBackend
 
-class RandomNumberGeneratorState: Observable {
+class NumberModeState: Observable {
     @Observed var minNum = 0
     @Observed var maxNum = 100
     @Observed var randomNumber = 0
@@ -18,7 +38,7 @@ struct RNGTool_Plus_App: App {
 
     let identifier = "com.NCX-Programming.RNGTool-Plus"
 
-    let state = RandomNumberGeneratorState()
+    let state = NumberModeState()
 
     let windowProperties = WindowProperties(
         title: "RNGTool Plus",
